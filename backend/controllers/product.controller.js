5f95440919d8d4a2dc72ff9fcb90ae3fa6e5554a
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-06-30 15:28:35 +0500

    feat(product): add product creation endpoint with Cloudinary image upload support

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
@@ -35,4 +36,30 @@ export const getFeaturedProducts = async (req, res) => {
   }
 };
 
-export const createProduct = async (req, res) => {};
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, image, category } = req.body;
+
+    let cloudinaryRespesponse = null;
+
+    if (image) {
+      cloudinaryRespesponse = await cloudinary.uploader.upload(image, {
+        folder: "products",
+      });
+    }
+
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      image: cloudinaryRespesponse?.secure_url
+        ? cloudinaryRespesponse.secure_url
+        : "",
+      category,
+    });
+
+    res.status(201).json({ product });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
