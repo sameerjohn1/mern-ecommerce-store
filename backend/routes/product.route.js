0cfe303e7a47829b909a0c6d48964cb07c4c8b81
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-06-30 15:41:13 +0500

    feat(product): add product deletion endpoint with Cloudinary image removal

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -3,6 +3,7 @@ import {
   getAllProducts,
   getFeaturedProducts,
   createProduct,
+  deleteProduct,
 } from "../controllers/product.controller.js";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
 
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.get("/", protectRoute, adminRoute, getAllProducts);
 router.get("/featured", getFeaturedProducts);
-router.post("/", createProduct);
+router.post("/", protectRoute, adminRoute, createProduct);
+router.delete("/:id", protectRoute, adminRoute, deleteProduct);
 
 export default router;
