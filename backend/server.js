8474c2bb18220852b6ae74f8bbd4df4888556888
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-07-05 06:43:22 +0500

    feat(payment): implement Stripe checkout session with coupon support

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is listen on port http://localhost:${PORT}`);
