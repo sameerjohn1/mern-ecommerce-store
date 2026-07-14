088d6fdfd3866709d48ad6af62b7bb81bf72a7d0
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-07-14 12:44:14 +0500

    feat(cart): implement shopping cart page and state management

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,13 +10,23 @@ import { useEffect } from "react";
 import LoadingSpinner from "./components/LoadingSpinner";
 import AdminPage from "./pages/AdminPage";
 import CategoryPage from "./pages/CategoryPage";
+import CartPage from "./pages/CartPage";
+import { useCartStore } from "./stores/useCartStore";
 
 const App = () => {
   const { user, checkAuth, checkingAuth } = useUserStore();
+  	const { getCartItems } = useCartStore();
+
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
+
+  	useEffect(() => {
+		if (!user) return;
+
+		getCartItems();
+	}, [getCartItems, user]);
 
   if (checkingAuth) return <LoadingSpinner />;
 
@@ -50,6 +60,9 @@ const App = () => {
           />
 
           <Route path="/category/:category" element={<CategoryPage />} />
+
+          <Route path='/cart' element={user ? <CartPage /> : <Navigate to='/login' />} />
+
         </Routes>
       </div>
 
